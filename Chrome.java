cdb44fe3ccdb06ccfe39a76d386604fb9d553c8a
Hema-git-design <125679859+Hema-git-design@users.noreply.github.com>
2026-04-25 15:34:01 +0530

    Add files via upload
    Day1.Assignment

---
+++ Chrome.java
@@ -0,0 +1,22 @@
+package org.testleaf;
+
+public class Chrome {
+
+    public static void main(String[] args) {
+
+        // Variable declarations and initializations
+        float version = 91.0f;
+        String developer = "Google";
+        boolean isBeta = false;
+        int releaseYear = 2008;
+        char shortcutKey = 'C';
+
+        // Printing variable values
+        System.out.println("Version: " + version);
+        System.out.println("Developer: " + developer);
+        System.out.println("Is Beta: " + isBeta);
+        System.out.println("Release Year: " + releaseYear);
+        System.out.println("Shortcut Key: " + shortcutKey);
+    }
+}
+    
